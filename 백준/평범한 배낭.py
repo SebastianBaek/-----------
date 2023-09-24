17dea062dc8c942918d22a025315bd4fcdde2b1a
SebastianBaek <gnsrudqor@naver.com>
2023-09-24 17:49:13 +0900

    평범한 배낭 풀이

---
+++ 백준/평범한 배낭.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/12865
+N, K = map(int, input().split())
+stuffs = [[0, 0]] + [tuple(map(int, input().split())) for i in range(N)]
+dp = [[0] * (K + 1) for i in range(N + 1)]
+
+for i in range(1, N + 1):
+    w, v = stuffs[i][0], stuffs[i][1]
+    for j in range(1, K + 1):
+        if j < w:
+            dp[i][j] = dp[i - 1][j]
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w] + v)
+
+print(dp[N][K])
